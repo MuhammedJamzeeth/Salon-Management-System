1d43af78906244c78aff935372db132949aa06c5
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-03-22 01:07:12 +0530

    Updated file type

--- salon-management-shop/src/main/java/com/jamsy/shop/model/ServiceModel.java
+++ salon-management-shop/src/main/java/com/jamsy/shop/model/ServiceModel.java
@@ -19,5 +19,5 @@ public class ServiceModel {
     private String serviceDescription;
     private double servicePrice;
     private boolean serviceStatus;
-    private byte[] imageData;
+    private String imageData;
 }
