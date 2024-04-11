e75e4f2b64df0c9038639d9154789725a9790492
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-04-11 18:48:12 +0530

    created product model

---
+++ SMS-Backend/src/main/java/com/jamsy/shop/model/ProductModel.java
@@ -0,0 +1,20 @@
+package com.jamsy.shop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductModel {
+
+    private Long productId;
+    private String productName;
+    private int productQty;
+    private Double productPrice;
+    private String productCategory;
+    private String expirationDate;
+    private String productStatus;
+
+}
