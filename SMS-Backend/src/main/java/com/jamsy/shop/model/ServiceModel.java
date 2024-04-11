a56b04c671b12c2d0210a21f9918dabe7ca05552
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-04-11 18:51:20 +0530

    updated service model

--- SMS-Backend/src/main/java/com/jamsy/shop/model/ServiceModel.java
+++ SMS-Backend/src/main/java/com/jamsy/shop/model/ServiceModel.java
@@ -1,25 +1,21 @@
 package com.jamsy.shop.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Data
-@Table(name="Service")
 public class ServiceModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "serviceId", nullable = false)
     private Long serviceId;
     private String serviceName;
     private String serviceDescription;
     private double servicePrice;
     private String serviceState;
     private Date serviceDate;
+    private List<ServiceModel> serviceModelList;
 }
