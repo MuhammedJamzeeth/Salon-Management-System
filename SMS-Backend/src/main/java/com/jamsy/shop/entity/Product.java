1ce67594b873539289c7470dc0c0a4f224085e05
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-04-11 18:45:36 +0530

    Create product entity

---
+++ SMS-Backend/src/main/java/com/jamsy/shop/entity/Product.java
@@ -0,0 +1,29 @@
+package com.jamsy.shop.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "Product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private String productName;
+    private int productQty; //track inventory level
+    private double productPrice;
+    private String productCategory;
+    private String expirationDate;
+    private String productStatus;
+
+}
+
