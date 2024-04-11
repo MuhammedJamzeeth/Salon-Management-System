70e863520515bbeaa220b03b2e35579b180db7b3
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-04-11 18:48:36 +0530

    created product service impl

---
+++ SMS-Backend/src/main/java/com/jamsy/shop/service/ProductServiceImpl.java
@@ -0,0 +1,89 @@
+package com.jamsy.shop.service;
+
+import com.jamsy.shop.entity.Product;
+import com.jamsy.shop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+//    @Override
+//    public Product getProductById(Long id) {
+//        return productRepository.findById(id).get();
+//    }
+
+    @Override
+    public Product saveProduct(Product product) {
+        Product p = new Product();
+        String productState = calculateProductState(product.getProductQty());
+        p.setProductName(product.getProductName());
+        p.setProductPrice(product.getProductPrice());
+        p.setProductQty(product.getProductQty());
+        p.setProductCategory(product.getProductCategory());
+        p.setExpirationDate(product.getExpirationDate());
+        p.setProductStatus(productState);
+
+        return productRepository.save(p);
+    }
+
+    // Helper method to calculate productState based on productQuantity
+    private String calculateProductState(int productQuantity) {
+        // Logic to determine productState based on productQuantity
+        if (productQuantity <= 0) {
+            return "Out of Stock";
+        } else if (productQuantity < 10) {
+            return "Running Low";
+        } else {
+            return "Full";
+        }
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        if(productRepository.existsById(id)){
+            productRepository.deleteById(id);
+        }else {
+            throw new IllegalArgumentException("Product not found");
+        }
+    }
+
+    @Override
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Product existingProduct = productRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("service with id " + id + " not found"));
+        // Calculate productState based on updated productQuantity
+        String productState = calculateProductState(updatedProduct.getProductQty());
+
+        if (updatedProduct.getProductName() != null) {
+            existingProduct.setProductName(updatedProduct.getProductName());
+        }
+        if (updatedProduct.getProductCategory() != null) {
+            existingProduct.setProductCategory(updatedProduct.getProductCategory());
+        }
+        if (updatedProduct.getProductQty() != 0) {
+            existingProduct.setProductQty(updatedProduct.getProductQty());
+        }
+        if (updatedProduct.getProductPrice() != 0) {
+            existingProduct.setProductPrice(updatedProduct.getProductPrice());
+        }
+        if (updatedProduct.getExpirationDate() != null) {
+            existingProduct.setExpirationDate(updatedProduct.getExpirationDate());
+        }
+        // Set productState based on calculation
+        if (updatedProduct.getProductStatus() != null){
+            existingProduct.setProductStatus(productState);
+        }
+
+        return productRepository.save(existingProduct);
+    }
+}
