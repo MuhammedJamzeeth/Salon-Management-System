c12193001c29ff52f36206bf1f78b9c708ae0ccb
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-05-04 21:52:42 +0530

    updated

--- sms-frontend-client/src/Components/ImageSlider.js
+++ sms-frontend-client/src/Components/ImageSlider.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './ImageSlider.css';
 import image1 from '../Resources/image1.jpg';
 import image2 from '../Resources/image2.jpg';
+import image3 from '../Resources/image3.png';
 
 function ImageSlider() {
     const [activeIndex, setActiveIndex] = useState(0);
-
-    const images = [image1, image2]; // Use your images array here
+    const images = [image1, image2, image3]; // Use your images array here
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             setActiveIndex(prevIndex => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-        }, 10000); // Run effect only once when component mounts
+        }, 3000); // Run effect only once when component mounts
         return () => clearInterval(intervalId);
-    }, []);
+    }, [images.length]);
 
     return (
         <div className="image-slider">
